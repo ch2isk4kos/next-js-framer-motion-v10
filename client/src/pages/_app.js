d6af0dc8bfb2d6a9d14120c445d1868c918c4bde
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-05 15:45:50 -0400

    add tailwind inline styles to root

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,7 @@
 // This file (component) is used to initialize the pages
-import "@/styles/globals.css";
+import Head from "next/head";
 import { Montserrat } from "next/font/google";
+import "@/styles/globals.css";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -9,9 +10,17 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${montserrat.variable} font-mont`}>
-      {/* active page */}
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        {/* active page */}
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
