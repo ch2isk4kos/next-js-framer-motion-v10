6e25afa8fbf59b5aa4829b0dceac0fe91237e231
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-05 15:33:07 -0400

    optimize google font usage through application

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+// This file (component) is used to initialize the pages
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-mont`}>
+      {/* active page */}
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
