18cd764aeaea37e91e335cde70b8e541a484761f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-06 14:11:00 -0400

    customize on hover effect of custom link(s)

--- client/src/components/nav/CustomLink.js
+++ client/src/components/nav/CustomLink.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
-  const linkStyles = `${className} relative group`;
-  const spanStyles =
-    "h-[2px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300";
+  const router = useRouter();
+  const linkStyles = `
+    ${className} relative group
+  `;
+  const spanStyles = `
+    h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5
+    group-hover:w-full transition-[width] ease duration-300
+    ${router.asPath === href ? "w-full" : "w-0"}
+  `;
 
   return (
     <Link href={href} className={linkStyles}>
