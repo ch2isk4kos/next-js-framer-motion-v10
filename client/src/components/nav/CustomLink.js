edff83ade4aa75d1c6720a7ebb7472e56fb613cc
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-06 14:01:43 -0400

    add on hover styling

--- client/src/components/nav/CustomLink.js
+++ client/src/components/nav/CustomLink.js
@@ -2,9 +2,14 @@ import React from "react";
 import Link from "next/link";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const linkStyles = `${className} relative group`;
+  const spanStyles =
+    "h-[2px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300";
+
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={linkStyles}>
       {title}
+      <span className={spanStyles}>&nbsp;</span>
     </Link>
   );
 };
