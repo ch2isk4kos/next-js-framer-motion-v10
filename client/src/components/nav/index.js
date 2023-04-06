dd91cc05deee7683e33216044cb9275ce7924615
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-06 16:29:19 -0400

    style right side icon svg(s)

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -11,12 +11,16 @@ import {
   CircularText,
   LinkArrow,
 } from "./Icons";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const headerStyles =
     "w-full px-32 py-8 font-medium flex items-center justify-between";
   const midNavStyles = "";
-  const rightNavStyles = "";
+  const rightNavStyles = "flex items-center justify-center flex-wrap";
+  const aStyles = "w-6";
+  const hoverEffect = { y: -4 };
+  const tapEffect = { scale: 0.9 };
 
   return (
     <header className={headerStyles}>
@@ -30,27 +34,78 @@ const Navbar = () => {
         <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
       <nav className={rightNavStyles}>
-        <Link href="/" target={"_blank"}>
-          <TwitterIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
-          <GitHubIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
+        <motion.a
+          href="https://chriskakos.medium.com/"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
+          <h1>M</h1>
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/chris-kakos"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
           <LinkedInIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
-          <SunIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
+        </motion.a>
+        <motion.a
+          href="https://github.com/ch2isk4kos"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
+          <GitHubIcon />
+        </motion.a>
+        <motion.a
+          href="https://twitter.com/ch2isk4kos"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
           <MoonIcon />
-        </Link>
-        <Link href="/" target={"_blank"}>
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
+          <SunIcon />
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          className={`${aStyles} mx-2`}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
           {/* <MoonIcon /> */}
-        </Link>
-        <Link href="/" target={"_blank"}>
+        </motion.a>
+        {/* <motion.a
+          href="/"
+          target={"_blank"}
+          className={``}
+          whileHover={hoverEffect}
+          whileTap={tapEffect}
+        >
           <LinkArrow />
-        </Link>
+        </motion.a> */}
       </nav>
     </header>
   );
