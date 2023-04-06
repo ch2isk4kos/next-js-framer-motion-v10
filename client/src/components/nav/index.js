c7e8f3fb457ed77f6009faf3005ca646efabf828
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-06 16:58:47 -0400

    remove comment(s)

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -16,9 +16,9 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   const headerStyles =
     "w-full px-32 py-8 font-medium flex items-center justify-between";
-  const midNavStyles = "";
-  const rightNavStyles = "flex items-center justify-center flex-wrap";
-  const aStyles = "w-6";
+  const pageNavStyles = "";
+  const iconNavStyles = "flex items-center justify-center flex-wrap";
+  const aStyles = "w-6 mx-2";
   const hoverEffect = { y: -4 };
   const tapEffect = { scale: 0.9 };
 
@@ -27,85 +27,83 @@ const Navbar = () => {
       <div>
         <Logo />
       </div>
-      <nav className={midNavStyles}>
+      <nav className={pageNavStyles}>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
         <CustomLink href="/projects" title="Projects" className="mx-4" />
         <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
-      <nav className={rightNavStyles}>
+      <nav className={iconNavStyles}>
+        {/* Medium */}
         <motion.a
           href="https://chriskakos.medium.com/"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <h1>M</h1>
         </motion.a>
+        {/* LinkedIn */}
         <motion.a
           href="https://www.linkedin.com/in/chris-kakos"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <LinkedInIcon />
         </motion.a>
+        {/* GitHub */}
         <motion.a
           href="https://github.com/ch2isk4kos"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <GitHubIcon />
         </motion.a>
+        {/* Twitter */}
         <motion.a
           href="https://twitter.com/ch2isk4kos"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <TwitterIcon />
         </motion.a>
+        {/* Sun? */}
         <motion.a
           href="/"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <MoonIcon />
         </motion.a>
+        {/* Moon? */}
         <motion.a
           href="/"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
+          className={`${aStyles}`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <SunIcon />
         </motion.a>
+        {/* Line Arrow */}
         <motion.a
           href="/"
           target={"_blank"}
-          className={`${aStyles} mx-2`}
-          whileHover={hoverEffect}
-          whileTap={tapEffect}
-        >
-          {/* <MoonIcon /> */}
-        </motion.a>
-        {/* <motion.a
-          href="/"
-          target={"_blank"}
-          className={``}
+          className={`w-6 ml-2`}
           whileHover={hoverEffect}
           whileTap={tapEffect}
         >
           <LinkArrow />
-        </motion.a> */}
+        </motion.a>
       </nav>
     </header>
   );
