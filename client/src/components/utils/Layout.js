ece7317655446bd0e1f662ba61c5fc28d1cec27b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 15:33:18 -0400

    create utils folder

--- client/src/components/utils/Layout.js
+++ client/src/components/utils/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Layout = ({ children, className = "" }) => {
   const containerStyles = `w-full h-full inline-block z-0 bg-light p-32 ${className}`;
+  //   const containerStyles = `${className}`;
   return (
     <div className={`${containerStyles}`}>
       {/*  */}
