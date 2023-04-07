94a0d7055648501edbbe93dfceb078fba8b40fe2
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 15:20:59 -0400

    create layout utility component

---
+++ client/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = ({ children, className = "" }) => {
+  const containerStyles = `w-full h-full inline-block z-0 bg-light p-32 ${className}`;
+  return (
+    <div className={`${containerStyles}`}>
+      {/*  */}
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
