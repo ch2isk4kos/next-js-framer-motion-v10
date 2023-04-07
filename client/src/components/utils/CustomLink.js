453a2cc8f96ad858d0f145bd2c5d742bad29e75a
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 17:20:12 -0400

    conditionally render component based on target prop value

--- client/src/components/utils/CustomLink.js
+++ client/src/components/utils/CustomLink.js
@@ -2,20 +2,23 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const CustomLink = ({ href, title, className = "" }) => {
+const CustomLink = ({ className = "", href, title, target = false }) => {
   const router = useRouter();
 
-  const linkStyles = `
-    ${className} relative group
-  `;
+  const linkStyles = `relative group ${className}`;
   const spanStyles = `
     h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5
     group-hover:w-full transition-[width] ease duration-300
     ${router.asPath === href ? "w-full" : "w-0"}
   `;
 
-  return (
-    <Link href={href} className={linkStyles}>
+  return target ? (
+    <Link className={linkStyles} href={href} target={"_blank"}>
+      {title}
+      <span className={spanStyles}>&nbsp;</span>
+    </Link>
+  ) : (
+    <Link className={linkStyles} href={href}>
       {title}
       <span className={spanStyles}>&nbsp;</span>
     </Link>
