3eb3ba3920608c6de07ee1c15e787fb9b74d42d6
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 21:35:43 -0400

    change naming convention of tailwind variable

--- client/src/components/utils/Layout.js
+++ client/src/components/utils/Layout.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 const Layout = ({ children, className = "" }) => {
-  const containerStyles = `w-full h-full inline-block z-0 bg-light p-32 ${className}`;
-  //   const containerStyles = `${className}`;
+  const layout = `w-full h-full inline-block z-0 bg-light p-32 ${className}`;
   return (
-    <div className={`${containerStyles}`}>
+    <div className={`${layout}`}>
       {/*  */}
       {children}
     </div>
