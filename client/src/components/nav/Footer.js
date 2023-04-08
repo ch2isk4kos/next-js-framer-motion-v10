600b77f2cfa913d33a64f926cc86b184dc1f674d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 23:09:06 -0400

    add trigram for water symbol

--- client/src/components/nav/Footer.js
+++ client/src/components/nav/Footer.js
@@ -5,15 +5,14 @@ import Layout from "../utils/Layout";
 const Footer = () => {
   const footer = `w-full border-t-2 border-solid border-dark font-medium text-lg`;
   const layout = `flex items-center justify-between py-8`;
-  const stack = `text-blue-700 text-4xl p-1 mb-1`;
+  const trigramForWater = `text-blue-700 text-4xl p-1 mb-1`;
   const p = `flex items-center`;
   return (
     <footer className={footer}>
       <Layout className={layout}>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <p className={p}>
-          {/* Built with <span className={star}>&#9734;</span> */}
-          <span className={stack}>&#9781;</span>
+          <span className={trigramForWater}>&#9781;</span>
           by&nbsp;
           <Link className={""} href={"/"}>
             Chris Kakos
