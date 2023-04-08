30e59e5cc83f9a11f29227834c2315d83c5baf9b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-07 20:41:27 -0400

    import and render footer

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,7 @@
 // This root file (component) is used to initialize the pages
 import Head from "next/head";
 import Navbar from "@/components/nav/index.js";
+import Footer from "@/components/nav/Footer";
 import { Montserrat } from "next/font/google";
 import "@/styles/globals.css";
 
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }) {
         <Navbar />
         {/* active page */}
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   );
