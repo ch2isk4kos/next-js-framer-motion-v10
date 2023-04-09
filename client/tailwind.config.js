5b57169750ad3726b89448353d980ff408452655
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 13:11:57 -0400

    add animation property + config key/value

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,14 +9,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        mont: ["var(--font-mont)", ...fontFamily.sans],
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
       },
       colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
+      },
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
     },
   },
