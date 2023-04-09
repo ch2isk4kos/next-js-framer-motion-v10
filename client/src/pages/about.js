ce731a33754e39310da7a5a36f42c36e1d29a6c6
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 13:54:13 -0400

    render biography section

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,8 +1,35 @@
 import React from "react";
 import Head from "next/head";
+import Layout from "@/components/utils/Layout";
 import AnimatedText from "@/components/utils/AnimatedText";
 
 const About = () => {
+  const layout = `pt-16`;
+  const text = `flex flex-column w-full  items-center justify-center font-medium text-7xl`;
+  const grid = `grid grid-cols-8 gap-16 w-full`;
+  const column = `flex flex-col col-span-3 items-start`;
+  const h2 = `mb-4 text-lg font-bold uppercase text-dark/75`;
+  const pg = `py-2 font-medium`;
+
+  const paragraphs = [
+    {
+      id: "0",
+      text: "blah blah blah blah. blah blah blah blah. blah blah blah blah. blah blah blah blah.",
+    },
+    {
+      id: "1",
+      text: "blah blah blah blah. blah blah blah blah. blah blah blah blah. blah blah blah blah.",
+    },
+    {
+      id: "2",
+      text: "blah blah blah blah. blah blah blah blah. blah blah blah blah. blah blah blah blah.",
+    },
+    {
+      id: "3",
+      text: "blah blah blah blah. blah blah blah blah. blah blah blah blah. blah blah blah blah.",
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -10,7 +37,21 @@ const About = () => {
         <meta name="description" content="full stack software engineer" />
       </Head>
       <main>
-        <AnimatedText text="About" />
+        <Layout className={layout}>
+          <AnimatedText className={text} text="About" />
+          <div className={grid}>
+            <div className={column}>
+              <h2 className={h2}>Biography </h2>
+              {paragraphs.map((paragraph) => (
+                <>
+                  <p className={pg} key={paragraph.id}>
+                    {paragraph.text}
+                  </p>
+                </>
+              ))}
+            </div>
+          </div>
+        </Layout>
       </main>
     </>
   );
