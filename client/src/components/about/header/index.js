a4fc815653f35b863208facf9cda1f47ec4be653
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 16:27:37 -0400

    add tailwind to column3

--- client/src/components/about/header/index.js
+++ client/src/components/about/header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Biography from "@/components/about/header/Biography";
+import Stats from "./Stats";
 import globe from "../../../../public/images/satelite-globe-1-vector.png";
 
 const grid = `grid grid-cols-8 gap-16 w-full`;
 const column1 = `flex flex-col col-span-3 items-start`;
 const column2 = `relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark`;
+const column3 = `flex flex-col col-span-2 items-end justify-between`;
 const image = `w-full h-auto rounded-3xl`;
 
 const Header = () => {
@@ -22,6 +24,9 @@ const Header = () => {
           width={500}
           height={500}
         />
+      </div>
+      <div className={column3}>
+        <Stats />
       </div>
     </div>
   );
