fdda1eaebbc2566dcd0906673323791a28298099
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 14:31:59 -0400

    create directory for tailwind exports

---
+++ client/src/styles-tailwind/home.js
@@ -0,0 +1,18 @@
+const smWindow = `
+    border-2 border-green-600
+`;
+
+const mdWindow = `
+    border-2 border-blue-800
+`;
+
+const lgWindow = `
+    border-2 border-red-600
+`;
+
+const xlWindow = `
+    border-2 border-yellow-800
+`;
+
+const windows = { smWindow, mdWindow, lgWindow, xlWindow };
+modules.export = { windows };
