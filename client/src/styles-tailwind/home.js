3009a53f0be34da072171058f42588c3f95669dd
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 14:45:30 -0400

    create header export(s)

---
+++ client/src/styles-tailwind/home.js
@@ -0,0 +1,24 @@
+const small = `
+    border-2 border-green-600
+`;
+
+const medium = `
+    border-2 border-blue-800
+`;
+
+const large = `
+    border-2 border-red-600
+`;
+
+const xLarge = `
+    border-2 border-yellow-800
+`;
+
+const container = `
+     border-2 border-blue-800
+`;
+
+const window = { small, medium, large, xLarge };
+const header = { container };
+
+modules.export = { window, header };
