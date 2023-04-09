2b53492b00bfe2840fb688d18fc71afccb709fff
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-09 13:11:31 -0400

    comment code

--- client/src/components/utils/Thumbnail.js
+++ client/src/components/utils/Thumbnail.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import Link from "next/link";
 import { CircularText } from "../nav/Icons";
 
 const Thumbnail = () => {
@@ -6,13 +7,21 @@ const Thumbnail = () => {
     fixed left-4 bottom-4
     flex items-center justify-center overflow-hidden
   `;
-  const circleTextWrapper = `w-48 h-auto flex items-center justify-center relative`;
-  const circleText = `fill-dark `;
+  const circleTextWrapper = `relative w-48 h-auto flex items-center justify-center`;
+  // const circleTextWrapper = `relative w-48 h-auto flex items-center justify-center animate-spin-slow`;
+  const circleText = `fill-dark`;
+  // const link = `
+  //   asbolute flex items-center justify-center
+  //   right-2 top-1/2 -translate-x-1/2 -translate-y-1/2
+  // `;
 
   return (
     <div className={thumbnail}>
       <div className={circleTextWrapper}>
         <CircularText className={circleText} />
+        {/* <Link className={link} href={`mailto:test@gmail.com`}>
+          Hire Me
+        </Link> */}
       </div>
     </div>
   );
