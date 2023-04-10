004216ec425a41ed3e16e6d9f1defad926591494
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-10 19:29:28 -0400

    import + render about page component(s)

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -3,9 +3,11 @@ import Head from "next/head";
 import Layout from "@/components/utils/Layout";
 import AnimatedText from "@/components/utils/AnimatedText";
 import Header from "@/components/about/header";
+import Skills from "@/components/about/skills";
+import Experience from "@/components/about/experience";
 
 const layout = `pt-16`;
-const text = `flex flex-column w-full items-center justify-center font-medium text-7xl`;
+const text = `flex flex-column w-full items-center justify-center mb-32 font-medium text-7xl uppercase`;
 
 const AboutPage = () => {
   return (
@@ -16,8 +18,10 @@ const AboutPage = () => {
       </Head>
       <main>
         <Layout className={layout}>
-          <AnimatedText className={text} text="About" />
+          {/* <AnimatedText className={text} text="About" /> */}
           <Header />
+          <Skills />
+          <Experience />
         </Layout>
       </main>
     </>
