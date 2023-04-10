004c08f23f5ead08a096d650eba988e755620fbb
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-10 19:28:38 -0400

    import + render about component(s)

--- client/src/components/about/header/index.js
+++ client/src/components/about/header/index.js
@@ -4,10 +4,10 @@ import Biography from "@/components/about/header/Biography";
 import Stats from "./Stats";
 import globe from "../../../../public/images/satelite-globe-1-vector.png";
 
-const grid = `grid grid-cols-8 gap-16 w-full`;
-const column1 = `flex flex-col col-span-3 items-start`;
-const column2 = `relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark`;
-const column3 = `flex flex-col col-span-2 items-end justify-between`;
+const grid = `grid grid-cols-8 gap-16 w-full border-2 border-blue-800`;
+const column1 = `flex flex-col col-span-3 items-start justify-around`;
+const column2 = `relative col-span-3 h-max my-8 rounded-2xl border-2 border-solid border-dark`;
+const column3 = `flex flex-col col-span-2 items-end justify-around`;
 const image = `w-full h-auto rounded-3xl`;
 
 const Header = () => {
