ca9263db4d06302c6e918ce280d3e8c7926b7e06
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-12 23:19:46 -0400

    add text color to company link

--- client/src/components/about/experience/Position.js
+++ client/src/components/about/experience/Position.js
@@ -3,8 +3,9 @@ import React from "react";
 // const li = `flex flex-col items-center justify-between w-[60%] mx-auto my-8 first:mt-0 last:mb-0`;
 const root = `flex flex-col mx-auto my-8 first:mt-0 last:mb-0`;
 const h3 = `w-full text-lg font-bold uppercase`;
+const a = `text-blue-600`;
 const duration = `w-full font-medium text-dark/75 uppercase`;
-// const p = ``;
+const summary = `font-medium w-full`;
 
 const Position = ({
   company,
@@ -18,15 +19,15 @@ const Position = ({
   return (
     <li className={root}>
       <h3 className={h3}>
-        <span>{title}</span>&nbsp;
-        <a href={companyURL} target={"_blank"}>
-          @{company}
+        <span>{title}</span>&nbsp; @
+        <a className={a} href={companyURL} target={"_blank"}>
+          {company}
         </a>
       </h3>
       <p className={duration}>
         {time} | {location}
       </p>
-      <span>
+      <span className={summary}>
         <br />
         <p>{overview}</p>
       </span>
