6c7fef8fbf7473d348e0718a0346fcae14ada1d8
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-19 11:55:41 -0400

    import positions array

--- client/src/components/about/experience/index.js
+++ client/src/components/about/experience/index.js
@@ -1,47 +1,11 @@
 import React from "react";
 import Position from "./Position";
+import { positions } from "./positions";
 
 const experience = `my-64`;
 const h2 = `font-bold text-8xl mb-32 w-full`;
 const ulWrapper = `relative w-[75%] mx-auto`;
 const ul = `flex flex-col items-start justify-between w-full ml-4`;
-
-const positions = [
-  {
-    title: "Associate Full Stack Engineer",
-    company: "Fortress Information Security",
-    companyURL: "https://www.fortressinfosec.com/home",
-    time: "March 2022 - May 2023",
-    location: "Orlando, FL/Remote",
-    overview:
-      "Responsible for developing and improving features for the Fortress M2V Marketplace, including building new tools for data analysis and visualization while providing accuracy, integrity and accessibility of compliance data.",
-    responsibilities: [
-      {
-        p1: "• bullet point 1",
-        p2: "• bullet point 2",
-        p3: "• bullet point 3",
-        p4: "• bullet point 4",
-      },
-    ],
-  },
-  {
-    title: "Help Desk Support Engineer",
-    company: "Robbins Research International",
-    companyURL: "https://www.tonyrobbins.com/",
-    time: "March 2017 - May 2018",
-    location: "San Diego, CA",
-    overview:
-      "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
-    responsibilities: [
-      {
-        p1: "• bullet point 1",
-        p2: "• bullet point 2",
-        p3: "• bullet point 3",
-        p4: "• bullet point 4",
-      },
-    ],
-  },
-];
 
 const Experience = () => {
   return (
