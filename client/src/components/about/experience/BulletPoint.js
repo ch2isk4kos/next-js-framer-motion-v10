246eb0d6d018fa21c316b5d8077ad764d95b2df8
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-19 14:02:31 -0400

    add animation to bullet point(s)

--- client/src/components/about/experience/BulletPoint.js
+++ client/src/components/about/experience/BulletPoint.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { motion, useScroll } from "framer-motion";
 
-const BulletPoint = () => {
+const circleWrapper = `absolute left-0 stroke-dark`;
+const circle = `stroke-blue-800 stroke-1 fill-none`;
+const circle2 = `stroke-[5px] fill-light`;
+const circle3 = `animate-pulse stroke-1 fill-blue-600`;
+
+const BulletPoint = ({ reference }) => {
+  const { scrollYProgress } = useScroll({
+    target: reference,
+    offset: ["center end", "center center"],
+  });
+
   return (
-    <div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
-    </div>
+    <figure className={circleWrapper}>
+      <svg width="75" height="75" viewBox="0 0 100 100">
+        <circle className={circle} cx="75" cy="50" r="20" />
+        <motion.circle
+          className={circle2}
+          cx="75"
+          cy="50"
+          r="20"
+          style={{ pathLength: scrollYProgress }}
+        />
+        <circle className={circle3} cx="75" cy="50" r="10" />
+      </svg>
+    </figure>
   );
 };
 
