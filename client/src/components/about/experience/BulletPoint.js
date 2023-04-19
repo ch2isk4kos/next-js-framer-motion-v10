a8cadebb045df2e708a08100ac0fbdf735e73753
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-19 13:22:10 -0400

    create bullet point component

---
+++ client/src/components/about/experience/BulletPoint.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BulletPoint = () => {
+  return (
+    <div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+
+export default BulletPoint;
