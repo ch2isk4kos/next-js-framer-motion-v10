93825460acce3293086a74c14c864a87ca7b3a3d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-19 13:21:41 -0400

    add data to test scroll animation

--- client/src/components/about/experience/positions.js
+++ client/src/components/about/experience/positions.js
@@ -17,6 +17,91 @@ export const positions = [
     ],
   },
   {
+    title: "Associate Full Stack Engineer",
+    company: "Fortress Information Security",
+    companyURL: "https://www.fortressinfosec.com/home",
+    time: "March 2022 - May 2023",
+    location: "Orlando, FL/Remote",
+    overview:
+      "Responsible for developing and improving features for the Fortress M2V Marketplace, including building new tools for data analysis and visualization while providing accuracy, integrity and accessibility of compliance data.",
+    responsibilities: [
+      {
+        p1: "• bullet point 1",
+        p2: "• bullet point 2",
+        p3: "• bullet point 3",
+        p4: "• bullet point 4",
+      },
+    ],
+  },
+  {
+    title: "Associate Full Stack Engineer",
+    company: "Fortress Information Security",
+    companyURL: "https://www.fortressinfosec.com/home",
+    time: "March 2022 - May 2023",
+    location: "Orlando, FL/Remote",
+    overview:
+      "Responsible for developing and improving features for the Fortress M2V Marketplace, including building new tools for data analysis and visualization while providing accuracy, integrity and accessibility of compliance data.",
+    responsibilities: [
+      {
+        p1: "• bullet point 1",
+        p2: "• bullet point 2",
+        p3: "• bullet point 3",
+        p4: "• bullet point 4",
+      },
+    ],
+  },
+  {
+    title: "Help Desk Support Engineer",
+    company: "Robbins Research International",
+    companyURL: "https://www.tonyrobbins.com/",
+    time: "March 2017 - May 2018",
+    location: "San Diego, CA",
+    overview:
+      "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
+    responsibilities: [
+      {
+        p1: "• bullet point 1",
+        p2: "• bullet point 2",
+        p3: "• bullet point 3",
+        p4: "• bullet point 4",
+      },
+    ],
+  },
+  {
+    title: "Help Desk Support Engineer",
+    company: "Robbins Research International",
+    companyURL: "https://www.tonyrobbins.com/",
+    time: "March 2017 - May 2018",
+    location: "San Diego, CA",
+    overview:
+      "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
+    responsibilities: [
+      {
+        p1: "• bullet point 1",
+        p2: "• bullet point 2",
+        p3: "• bullet point 3",
+        p4: "• bullet point 4",
+      },
+    ],
+  },
+  {
+    title: "Help Desk Support Engineer",
+    company: "Robbins Research International",
+    companyURL: "https://www.tonyrobbins.com/",
+    time: "March 2017 - May 2018",
+    location: "San Diego, CA",
+    overview:
+      "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
+    responsibilities: [
+      {
+        p1: "• bullet point 1",
+        p2: "• bullet point 2",
+        p3: "• bullet point 3",
+        p4: "• bullet point 4",
+      },
+    ],
+  },
+  {
     title: "Help Desk Support Engineer",
     company: "Robbins Research International",
     companyURL: "https://www.tonyrobbins.com/",
