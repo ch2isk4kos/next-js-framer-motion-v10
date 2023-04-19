17d72475411f7a13a8079837a412660d5ad7623e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-19 14:11:32 -0400

    add tailwind to svg element

--- client/src/components/about/experience/BulletPoint.js
+++ client/src/components/about/experience/BulletPoint.js
@@ -2,6 +2,7 @@ import React from "react";
 import { motion, useScroll } from "framer-motion";
 
 const circleWrapper = `absolute left-0 stroke-dark`;
+const svg = `-rotate-90`;
 const circle = `stroke-blue-800 stroke-1 fill-none`;
 const circle2 = `stroke-[5px] fill-light`;
 const circle3 = `animate-pulse stroke-1 fill-blue-600`;
@@ -14,7 +15,7 @@ const BulletPoint = ({ reference }) => {
 
   return (
     <figure className={circleWrapper}>
-      <svg width="75" height="75" viewBox="0 0 100 100">
+      <svg className={svg} width="75" height="75" viewBox="0 0 100 100">
         <circle className={circle} cx="75" cy="50" r="20" />
         <motion.circle
           className={circle2}
