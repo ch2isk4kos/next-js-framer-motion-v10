01b6db28f341f063b66c37193fc1871e70599424
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-20 15:30:07 -0400

    change file name

---
+++ client/src/components/projects/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "React Tetris",
+    url: "https://github.com/ch2isk4kos/react-tetris-2",
+  },
+  {
+    title: "E-com-mern",
+    url: "https://github.com/ch2isk4kos/e-com-mern",
+  },
+];
