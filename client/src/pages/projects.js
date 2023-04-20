a4709b1d2d2fbda7b0ec06da2505c408853d730f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-20 14:39:13 -0400

    import utility component(s)

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import Layout from "@/components/utils/Layout";
+import AnimatedText from "@/components/utils/AnimatedText";
+
+const layout = `pt-16`;
+const text = `
+  flex flex-column items-center justify-center
+  w-full mb-32 font-medium text-7xl uppercase
+`;
 
 const ProjectsPage = () => {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <>
+      <Head>
+        <title>Chris Kakos | Projects</title>
+        <meta name="description" content="full stack software engineer" />
+      </Head>
+      <main>
+        <Layout className={layout}>
+          <AnimatedText className={text} text="Projects" />
+        </Layout>
+      </main>
+    </>
   );
 };
 
