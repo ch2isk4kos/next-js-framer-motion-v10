b2db34eaf5b6d45360e15d1e60264020dd884f6e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-20 13:29:26 -0400

    create education directory + root component

---
+++ client/src/components/about/education/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const root = ``;
+const h2 = `font-bold text-8xl mb-32 w-full`;
+
+const Education = () => {
+  return (
+    <div className={root}>
+      <h2 className={h2}>Education</h2>
+    </div>
+  );
+};
+
+export default Education;
