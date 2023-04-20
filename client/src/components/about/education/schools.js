314bf6856656fa555588b47b902580ba49ad29f1
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-20 14:31:39 -0400

    create export for schools attended

---
+++ client/src/components/about/education/schools.js
@@ -0,0 +1,20 @@
+export const schools = [
+  {
+    name: "Flatiron School",
+    focus: "Software Engineering",
+    schoolURL: "https://flatironschool.com/",
+    time: "January 2019 - May 2020",
+    location: "New York, NY/Remote",
+    overview:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+  {
+    name: "County College of Morris",
+    focus: "Computer Science",
+    schoolURL: "https://www.ccm.edu/",
+    time: "September 2016 - 2018",
+    location: "Randolph, NJ",
+    overview:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+];
