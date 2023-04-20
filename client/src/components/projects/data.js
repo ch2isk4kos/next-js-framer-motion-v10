532ad9818571f659da7447d4feb461e8d8caeba8
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-20 16:14:57 -0400

    add keys to featured project property

--- client/src/components/projects/data.js
+++ client/src/components/projects/data.js
@@ -1,10 +1,23 @@
+export const featured = {
+  category: "Portfolio Application",
+  description:
+    "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  gitHub: "https://github.com/ch2isk4kos/next-js-framer-motion-v10",
+  url: "https://github.com/ch2isk4kos/next-js-framer-motion-v10",
+  title: "Chris Kakos Portfolio",
+};
+
 export const projects = [
   {
     title: "React Tetris",
     url: "https://github.com/ch2isk4kos/react-tetris-2",
+    category: "Games",
+    description: "",
   },
   {
     title: "E-com-mern",
     url: "https://github.com/ch2isk4kos/e-com-mern",
+    category: "E-commerce",
+    description: "",
   },
 ];
