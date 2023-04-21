8af0dbdad659125f1fb7eea417910d1aec49229d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-21 12:01:28 -0400

    add children component(s)

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,16 +1,38 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import framer from "../../../public/images/framer-motion-vector.png";
+import { GitHubIcon } from "../nav/Icons";
 
-const image = ``;
+const root = ``;
+const imageLink = `w-full cursor-pointer overflow-hidden rounded-lg`;
+const image = `w-full h-auto`;
+const projectInfo = `flex flex-col items-start justify-between w-1/2 pl-6`;
+const h2 = `font-medium text-center text-blue-600 text-2xl`;
+const type = `w-full my-2 font-bold text-left text-dark text-4xl`;
+const overview = `my-2 font-medium text-dark`;
+const links = `flex items-center mt-2`;
+const gitHubIcon = "w-12";
+const appLink = `ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold`;
 
-const Project = ({ category, gitHub, img, title, url }) => {
+const Project = ({ category, description, gitHub, img, title, url }) => {
   return (
-    <article>
-      <Link href={url} target="_blank">
-        <Image className={image} src={framer} alt={title} />
+    <article className={root}>
+      <Link className={imageLink} href={url} target="_blank">
+        <Image className={image} src={img} alt={title} />
       </Link>
+      <div className={projectInfo}>
+        {/* <h2 className={h2}>Featured Project</h2> */}
+        <span className={type}>{category}</span>
+        <p className={overview}>{description}</p>
+        <div className={links}>
+          {/* <Link href={gitHub} target="_blank">
+            <GitHubIcon className={gitHubIcon} />
+          </Link>
+          <Link href={url} target="_blank">
+            <p className={appLink}>Visit Website</p>
+          </Link> */}
+        </div>
+      </div>
     </article>
   );
 };
