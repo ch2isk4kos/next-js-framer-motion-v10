c314bed99c66ac55ccdf1d6cd42e54097270485c
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-21 12:55:46 -0400

    add tailwind to root element

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -3,15 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { GitHubIcon } from "../nav/Icons";
 
-const root = ``;
+const root = `
+  flex items-center justify-center w-full rounded-2xl
+  border border-solid border-dark bg-light p-6
+`;
 const imageLink = `w-full cursor-pointer overflow-hidden rounded-lg`;
 const image = `w-full h-auto`;
-const projectInfo = `flex flex-col items-start justify-between w-1/2 pl-6`;
+const projectInfo = `flex flex-col items-start justify-between w-full pl-6`;
 const h2 = `font-medium text-center text-blue-600 text-2xl`;
-const type = `w-full my-2 font-bold text-left text-dark text-2xl`;
+const type = `w-full my-2 font-bold text-left text-dark text-xl`;
 const overview = `my-2 font-medium text-dark`;
 const links = `flex items-center mt-2`;
-const gitHubIcon = "w-12";
+const gitHubIcon = "w-18 h-10";
 const appLink = `ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold`;
 
 const Project = ({ category, description, gitHub, img, title, url }) => {
@@ -29,7 +32,7 @@ const Project = ({ category, description, gitHub, img, title, url }) => {
             <GitHubIcon className={gitHubIcon} />
           </Link>
           <Link href={url} target="_blank">
-            <p className={appLink}>Visit Website</p>
+            <p className={appLink}>Website</p>
           </Link>
         </div>
       </div>
