aae0b62aa99d3ff3e7b8f944c2fe3f77e9ec0394
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-21 11:27:55 -0400

    pass prop(s) to image component

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import framer from "../../../public/images/framer-motion-vector.png";
+
+const image = ``;
 
 const Project = ({ category, gitHub, img, title, url }) => {
   return (
     <article>
       <Link href={url} target="_blank">
-        <Image />
+        <Image className={image} src={framer} alt={title} />
       </Link>
     </article>
   );
