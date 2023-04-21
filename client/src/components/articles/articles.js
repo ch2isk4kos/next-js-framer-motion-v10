63399304f1d937e5db4679216d72c7ec9461b81f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-21 14:31:10 -0400

    create article component

---
+++ client/src/components/articles/articles.js
@@ -0,0 +1,41 @@
+export const featured = {
+  title: "Raspberry Pi: Installing Retro Pie",
+  url: "https://chriskakos.medium.com/installing-retropie-onto-raspberry-pi-95070cf9612f",
+  description:
+    "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+};
+
+export const articles = [
+  {
+    id: "0",
+    img: ``,
+    title: "React: Passing Props",
+    url: "https://github.com/ch2isk4kos/react-tetris-2",
+    description:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+  {
+    id: "1",
+    img: ``,
+    title: "Go: An Overview",
+    url: "https://github.com/ch2isk4kos/e-com-mern",
+    description:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+  {
+    id: "2",
+    img: ``,
+    title: "JavaScript Algorithms: Palindrome Checker",
+    url: "https://github.com/ch2isk4kos/hexadb-cli",
+    description:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+  {
+    id: "3",
+    img: ``,
+    title: "Building A Rails API",
+    url: "https://github.com/ch2isk4kos/pwa-demo",
+    description:
+      "blah blah blah blah blah blah blah blah blah blah blah blah blah",
+  },
+];
