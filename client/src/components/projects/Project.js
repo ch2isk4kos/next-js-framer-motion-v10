53f4c0e402d2728695596ac83d4f00f0637d09f7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-21 12:12:45 -0400

    alter tailwind variable

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -8,7 +8,7 @@ const imageLink = `w-full cursor-pointer overflow-hidden rounded-lg`;
 const image = `w-full h-auto`;
 const projectInfo = `flex flex-col items-start justify-between w-1/2 pl-6`;
 const h2 = `font-medium text-center text-blue-600 text-2xl`;
-const type = `w-full my-2 font-bold text-left text-dark text-4xl`;
+const type = `w-full my-2 font-bold text-left text-dark text-2xl`;
 const overview = `my-2 font-medium text-dark`;
 const links = `flex items-center mt-2`;
 const gitHubIcon = "w-12";
@@ -21,16 +21,16 @@ const Project = ({ category, description, gitHub, img, title, url }) => {
         <Image className={image} src={img} alt={title} />
       </Link>
       <div className={projectInfo}>
-        {/* <h2 className={h2}>Featured Project</h2> */}
+        <h2 className={h2}>{title}</h2>
         <span className={type}>{category}</span>
         <p className={overview}>{description}</p>
         <div className={links}>
-          {/* <Link href={gitHub} target="_blank">
+          <Link href={gitHub} target="_blank">
             <GitHubIcon className={gitHubIcon} />
           </Link>
           <Link href={url} target="_blank">
             <p className={appLink}>Visit Website</p>
-          </Link> */}
+          </Link>
         </div>
       </div>
     </article>
