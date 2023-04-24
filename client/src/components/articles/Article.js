21e7e6ae658ce3712eb51fcbc2d0612872a7aaa0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-04-24 11:44:23 -0400

    create article component

--- client/src/components/articles/Article.js
+++ client/src/components/articles/Article.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-const Article = ({ description, img, title, url }) => {
+const Article = ({ description, img, medium, title, url }) => {
   return (
-    <div>
-      {/*  */}
-      <h2>{title}</h2>
-    </div>
+    <article className={root}>
+      {/* <Link className={imageLink} href={url} target="_blank">
+        <Image className={image} src={framer} alt={title} />
+      </Link> */}
+      <div className={articleInfo}>
+        <h2 className={h2}>{title}</h2>
+        <p className={preview}>{description}</p>
+        <div className={links}>
+          {/* <Link href={medium} target="_blank">
+            <p>Medium</p>
+          </Link> */}
+          {/* <Link href={url} target="_blank">
+            <p className={articleLink}>Read More</p>
+          </Link> */}
+        </div>
+      </div>
+    </article>
   );
 };
 
