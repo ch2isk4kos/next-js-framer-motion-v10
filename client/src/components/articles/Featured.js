44b7c709e29d385ea17c21be8e4450cad086f08d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-16 14:49:19 -0400

    comment code

--- client/src/components/articles/Featured.js
+++ client/src/components/articles/Featured.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import framer from "../../../public/images/framer-motion-vector.png";
-// import { GitHubIcon } from "../nav/Icons";
+
+const FramerImage = motion(Image);
 
 const root = `
   flex items-center justify-between w-full
   rounded-3xl border border-solid border-dark bg-light shadow-2xl mt-8 p-12
 `;
-const imageLink = `w-1/2 cursor-pointer overflow-hidden rounded-lg`;
+const imageLink = `w-1/2 inline-block rounded-lg cursor-pointer overflow-hidden`;
 const image = `w-full h-auto`;
 const articleInfo = `flex flex-col items-start justify-between w-1/2 pl-6`;
 const title = `w-full my-2 font-bold text-left text-dark text-4xl`;
@@ -21,7 +23,13 @@ const Featured = ({ data }) => {
   return (
     <article className={root}>
       <Link className={imageLink} href={data.url} target="_blank">
-        <Image className={image} src={framer} alt={data.title} />
+        <FramerImage
+          className={image}
+          src={framer}
+          alt={data.title}
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className={articleInfo}>
         <h2 className={h2}>Featured Article</h2>
