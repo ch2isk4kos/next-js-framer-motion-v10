a21c1aba1bee226fdda72710686339a401557449
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-16 14:52:33 -0400

    import/render latest articles

--- client/src/pages/articles.js
+++ client/src/pages/articles.js
@@ -4,7 +4,7 @@ import Layout from "@/components/utils/Layout";
 import Article from "@/components/articles/Article";
 import AnimatedText from "@/components/utils/AnimatedText";
 import Featured from "@/components/articles/Featured";
-import { featured, articles } from "@/components/articles/articles";
+import { featured, latest, articles } from "@/components/articles/articles";
 
 const layout = `pt-16`;
 const main = `flex flex-col items-center justify-center w-full mb-16 overflow-hidden`;
@@ -12,9 +12,10 @@ const text = `
   flex flex-column items-center justify-center
   w-full mb-2 font-bold text-6xl uppercase
 `;
-const doc = `absolute right-60 text-lg font-medium`;
+const docQuote = `absolute right-60 text-lg font-medium`;
 const grid = `grid grid-cols-12 gap-24 mt-14`;
 const featureGrid = `col-span-12`;
+const latestGrid = `col-span-4`;
 const articleGrid = `col-span-6`;
 
 const ArticlesPage = () => {
@@ -31,13 +32,24 @@ const ArticlesPage = () => {
         <Layout className={layout}>
           <AnimatedText className={text} text="''Where We're Going," />
           <AnimatedText className={text} text=" We Don't Need Roads.''" />
-          <span className={doc}>
+          <span className={docQuote}>
             <em>- Dr. Emmett Brown</em>
           </span>
           <div className={grid}>
             <div className={featureGrid}>
               <Featured data={featured} />
             </div>
+            {latest?.map((article) => (
+              <div className={latestGrid}>
+                <Article
+                  key={latest.id}
+                  img={article.img}
+                  summary={article.summary}
+                  title={article.title}
+                  url={article.url}
+                />
+              </div>
+            ))}
             {articles?.map((article) => (
               <div className={articleGrid} key={article.id}>
                 <Article
