1896689aef465f4ecdf211e26a916ed694084b5b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-19 19:04:24 -0400

    add tailwind to all articles grid

--- client/src/pages/articles.js
+++ client/src/pages/articles.js
@@ -18,7 +18,8 @@ const grid = `grid grid-cols-12 gap-24 mt-14`;
 const featureGrid = `col-span-12`;
 const latestGrid = `col-span-4`;
 // const latestGrid = `col-span-6`;
-const articleGrid = `col-span-6`;
+// const articleGrid = `col-span-6`;
+const articleGrid = `col-span-12`;
 
 const ArticlesPage = () => {
   return (
