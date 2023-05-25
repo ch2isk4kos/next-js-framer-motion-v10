342d14e9303c53ff36bb75536c46334b32b75bac
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-25 17:13:43 -0400

    render moving image utility component

--- client/src/components/articles/Article.js
+++ client/src/components/articles/Article.js
@@ -38,6 +38,7 @@ const Article = ({ img, medium, summary, title, url }) => {
         <h2 className={h2}>{title}</h2>
         <p className={preview}>{summary}</p>
       </div>
+      <MovingImage img={img} url={url} title={title} />
       <span className={date}>date</span>
     </article>
   );
