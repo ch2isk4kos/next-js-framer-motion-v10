17c0dee55bd7972d89ff184a0505c6d6b6f50cc2
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-29 13:46:43 -0400

    import useRef native react hook

--- client/src/components/utils/MovingImage.js
+++ client/src/components/utils/MovingImage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { motion, useMotionValue } from "framer-motion";
