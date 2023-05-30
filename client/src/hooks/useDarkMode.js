563b6762f3b0d249637cf3c68a4a172a87a5701c
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-30 15:24:15 -0400

    handleOnQuery: remove event listener to change on return

--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -26,6 +26,7 @@ const useDarkMode = () => {
     };
 
     mediaQuery.addEventListener("change", handleOnQuery);
+    return () => mediaQuery.removeEventListener("change");
   }, []);
 
   return (
