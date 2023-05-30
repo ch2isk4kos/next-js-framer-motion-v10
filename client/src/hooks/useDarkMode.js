bbf08d8a42ff1b14c97fa3ead782f23544a2b19d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-30 14:43:48 -0400

    init mode state to an empty string

--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const useDarkMode = () => {
   const darkModeQuery = "{prefer-color-scheme: dark}";
+  const [mode, setMode] = useState("");
   return (
     //
     <div>useDarkMode</div>
