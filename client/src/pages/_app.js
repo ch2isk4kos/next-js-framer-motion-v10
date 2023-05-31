99128d1cb5b3a020c2939f7347b1412862f9defc
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-31 11:01:44 -0400

    add tailwind attribute to main element for dark mode

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -11,14 +11,14 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }) {
-  const mainStyles = `font-mont bg-light w-full min-h-screen`;
+  const root = `w-full min-h-screen font-mont bg-light dark:bg-dark`;
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} ${mainStyles}}`}>
+      <main className={`${montserrat.variable} ${root}`}>
         <Navbar />
         {/* active page */}
         <Component {...pageProps} />
