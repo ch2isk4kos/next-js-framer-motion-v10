01f2ff9fc878921d6a2893226c4db199d16bb3e9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-31 12:02:48 -0400

    change text to light on dark mode

--- client/src/components/home/Header.js
+++ client/src/components/home/Header.js
@@ -6,18 +6,18 @@ import AnimatedText from "@/components/utils/AnimatedText";
 import ProfileImage from "../../../public/images/mobile-dev-1-vector.png";
 
 const Header = () => {
-  const root = `flex items-center justify-between w-[100%] min-h-screen`;
+  const root = `flex items-center justify-between w-[100%] min-h-screen dark:text-light`;
   const imgWrapper = `w-1/2 flex flex-col items-center self-center`;
   const img = `w-full h-auto`;
   const headingWrapper = `w-1/2`;
-  const heading = `inline-block w-full text-dark capitalize text-left`;
+  const heading = `inline-block w-full text-dark capitalize text-left dark:text-light`;
   const headingSubtext = `my-4 text-base font-medium`;
-  const buttonWrapper = `flex items-center self-start mt-2`;
+  const buttonWrapper = `flex items-center self-start mt-2 dark:text-light`;
   const button = `
     flex items-center text-light p-2.5 px-6 mx-2
     text-lg font-semibold bg-dark
     border-2 border-solid border-transparent rounded-lg 
-    hover:border-dark hover:bg-light hover:text-dark
+    hover:border-dark hover:bg-light hover:text-dark dark:border-light
   `;
 
   return (
