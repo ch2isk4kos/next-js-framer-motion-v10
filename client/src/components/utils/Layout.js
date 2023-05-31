77d96d7f7dec6183e89389992aec0699fdc350b4
ch2isk4kos <ch2isk4kos@gmail.com>
2023-05-31 10:21:24 -0400

    remove comment(s)

--- client/src/components/utils/Layout.js
+++ client/src/components/utils/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Layout = ({ children, className = "" }) => {
-  const layout = `w-full h-full inline-block z-0 bg-light p-32 ${className}`;
+  const root = `w-full max-h-screen inline-block z-0 bg-light p-32 dark:bg-dark ${className}`;
+  // const root = `w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark ${className}`;
+  // const root = `w-full min-h-screen inline-block z-0 bg-light p-32 dark:bg-dark ${className}`;
   return (
-    <div className={`${layout}`}>
+    <div className={root}>
       {/*  */}
       {children}
     </div>
